baed9f38dcf0e5fd533b43ad782bdbc476a30d0c
abhishek 1199 <abhishekshaw1199@gmail.com>
2024-01-07 18:17:35 +0530

    weather app

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Weather from './Component/Weather';
+function App() {
+  return (
+    <React.Fragment>
+    <Weather/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ weather-app/src/Component/Weather.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { FaStreetView } from "react-icons/fa";
+//import './App.css';
+const Weather = () => {
+    const [location, setLocation] = useState(null);
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=704e17eb03599e4b4d1413c267bd70e9`
+            const res = await fetch(url)
+            const resJson = await res.json();
+            //console.log(resJson)
+            setLocation(resJson.main)
+        }
+        fetchApi()
+    }, [search])
+
+    return (
+        <React.Fragment>
+            <div className='Main'>
+                <div className='Main-data'>
+                    <input type='search' placeholder="type your place" onChange={(e) => {
+                        setSearch(e.target.value);
+                    }}
+                        value={search} />
+                </div>
+
+                {!location ? (
+                    <p>No Data Found</p>
+                ) : (
+                    <div>
+                        <div className='Information'>
+                            <h1 className='locat'>
+                                <FaStreetView /> {search}
+                            </h1>
+                            <h2 className='celius'>
+                                {location.temp}°C
+                            </h2>
+
+                            <h3 className='max-min'>
+                                {location.temp_min}°C| {location.temp_max}°C
+                            </h3>
+                        </div>
+                    </div>
+                )}
+
+
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Weather
